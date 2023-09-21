20c6c74341ce103b029f918e31755cf2fd1da6ab
Dmitry Karpunin <kotel77@inbox.ru>
2023-09-21 13:57:14 +0300

    moving custom services from program.cs to class DependencyInjection

---
+++ Mardul.Recipes.Api/Configuration/DependencyInjection.cs
@@ -0,0 +1,36 @@
+﻿using Mardul.Recipes.Api.Services;
+using Mardul.Recipes.Core.Interfaces.Repositories;
+using Mardul.Recipes.Core.Interfaces.Services;
+using Mardul.Recipes.Core.Services;
+using Mardul.Recipes.Infrastructure.DbContexts;
+using Mardul.Recipes.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mardul.Recipes.Api.Configuration
+{
+    public static class DependencyInjection
+    {
+
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(x =>
+            {
+                x.UseSqlite(configuration.GetConnectionString("db"));
+                x.LogTo(message => System.Diagnostics.Debug.WriteLine(message));
+                x.UseLazyLoadingProxies();
+
+            });
+
+            services.AddAutoMapper(typeof(RecipeMappingProfile));
+
+            services.AddScoped<DbContext, AppDbContext>();
+
+            services.AddTransient<IRecipeIngredientRepository, RecipeIngredientRepository>();
+            services.AddTransient<IRecipeRepository, RecipeRepository>();
+            services.AddTransient<IUnitOfWorkService, UnitOfWorkService>();
+            services.AddTransient<IRecipeService, RecipeService>();
+
+            return services;
+        }
+    }
+}
--- Mardul.Recipes.Api/Program.cs
+++ Mardul.Recipes.Api/Program.cs
@@ -1,35 +1,13 @@
-using Mardul.Recipes.Api.Services;
-using Mardul.Recipes.Core.Interfaces.Services;
-using Mardul.Recipes.Core.Interfaces.Repositories;
-using Mardul.Recipes.Infrastructure.DbContexts;
-using Mardul.Recipes.Infrastructure.Repositories;
-using Microsoft.EntityFrameworkCore;
-using Mardul.Recipes.Core.Services;
+using Mardul.Recipes.Api.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Services.AddDbContext<AppDbContext>(x =>
-{
-    x.UseSqlite(builder.Configuration.GetConnectionString("db"));
-    x.LogTo(message => System.Diagnostics.Debug.WriteLine(message));
-    x.UseLazyLoadingProxies();
-
-});
-
-builder.Services.AddAutoMapper(typeof(RecipeMappingProfile));
-builder.Services.AddScoped<DbContext, AppDbContext>();
-builder.Services.AddTransient<IRecipeIngredientRepository, RecipeIngredientRepository>();
-builder.Services.AddTransient<IRecipeRepository, RecipeRepository>();
-builder.Services.AddTransient<IUnitOfWorkService, UnitOfWorkService>();
-builder.Services.AddTransient<IRecipeService, RecipeService>();
-
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
