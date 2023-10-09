3be14ebb242528be93df20a986e09a278161582c
Dmitry Karpunin <kotel77@inbox.ru>
2023-10-09 18:27:45 +0300

    fixing test

--- Mardul.Recipes.Tests/Services/PasswordHashServiceTest.cs
+++ Mardul.Recipes.Tests/Services/PasswordHashServiceTest.cs
@@ -1,38 +1,35 @@
-﻿
-using FakeItEasy;
+﻿using FakeItEasy;
 using FluentAssertions;
-using Mardul.Recipes.Core.Interfaces.Services;
 using Mardul.Recipes.Infrastructure.Authentication;
-using Microsoft.AspNetCore.Identity;
+using Mardul.Recipes.Infrastructure.Services;
+using Microsoft.Extensions.Options;
 using System.Security.Cryptography;
 
 namespace Mardul.Recipes.Tests.Services
 {
     public class PasswordHashServiceTest
     {
-        private readonly PasswordHashOptions _options;
+        private readonly PasswordHashService _passwordHashService;
+        private readonly IOptions<PasswordHashOptions> _options;
         public const string _password = "Qwerty123!";
         public const string _hashPassword = "0qcVx8uFFSm1VMlrFfYd0g==;lINnHFdzZYWdKcn9cCgnIfwSgh8UmPj9fF6UmDjY1Pk=";
         public PasswordHashServiceTest()
         {
-           _options = new PasswordHashOptions() 
-           { 
-               SaltSize = 16,
-               SaltDelimeter = ';',
-               Iterations = 1000,
-               KeySize = 32
-           };
+            _options = Options.Create(new PasswordHashOptions 
+            {
+                SaltSize = 16,
+                KeySize= 32,
+                SaltDelimeter = ';',
+                Iterations = 1000
+            });
+            _passwordHashService = new PasswordHashService(_options);
         }
 
         [Theory]
         [InlineData(_password)]
         public async Task PasswordHashService_Generate_returnToken(string password)
         {
-            var salt = RandomNumberGenerator.GetBytes(_options.SaltSize);
-
-            var hash = Rfc2898DeriveBytes.Pbkdf2(password, salt, _options.Iterations, _options.HashAlgorithmName, _options.KeySize);
-
-            var result = string.Join(_options.SaltDelimeter, Convert.ToBase64String(salt), Convert.ToBase64String(hash));
+            var result = _passwordHashService.Generate(password);
 
             result.Should().NotBeNull();
             result.Should().BeOfType<string>();
@@ -44,14 +41,7 @@ namespace Mardul.Recipes.Tests.Services
         public async Task PasswordHashService_Validate_returnTrue(string password, string passwordHash)
         {
 
-            var pwdElements = passwordHash.Split(_options.SaltDelimeter);
-
-            var salt = Convert.FromBase64String(pwdElements[0]);
-            var hash = Convert.FromBase64String(pwdElements[1]);
-
-            var hashInput = Rfc2898DeriveBytes.Pbkdf2(password, salt, _options.Iterations, _options.HashAlgorithmName, _options.KeySize);
-
-            var result = CryptographicOperations.FixedTimeEquals(hash, hashInput);
+            var result = _passwordHashService.Validate(password, passwordHash);
 
             result.Should().BeTrue();
 
